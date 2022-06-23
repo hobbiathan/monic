8c2327e3062099912c8a17208751f0a0085ed585
hobbiathan <hobbiathan@gmail.com>
2022-06-23 06:13:47 -0600

    add lib/runner.rb

---
+++ lib/runner.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+require './classes/block'
+require './classes/blockchain'
+
+
+monic = Blockchain.new
+monic.add_block(Block.new(1, "06/23/2022", { amount: 17 } ))
+monic.add_block(Block.new(2, "06/23/2022", { amount: 20 } )) 
+
+binding.pry
+
+monic.chain[1].udata({amount: 4000})
+monic.chain[1].hash = monic.chain[1].calculate_hash
+
+binding.pry
